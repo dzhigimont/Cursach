8320c898d923b9c95b73963075e7c428ba2d6106
dzhigimont <dzhigimont@mail.ru>
2016-07-18 13:01:22 +0300

    Update NewsListForm.java

--- src/test/java/forms/NewsListForm.java
+++ src/test/java/forms/NewsListForm.java
@@ -8,10 +8,19 @@ import webdriver.elements.ListOfPages;
 public class NewsListForm extends BaseForm {
 
     private ListOfPages newsList = new ListOfPages(By.xpath("//h3[@class='b-posts-1-item__title']/a"));
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public NewsListForm() {
         super(By.xpath("//h3[contains(text(),'Самое актуальное')]"), "NewsList.logo");
     }
-
+    /**
+     * clickFirstNews()
+     *this method clicks first link in the list news
+     *
+     * @return void
     public void clickFirstNews(){
         logger.info("==========The title news==========");
         logger.info(newsList.getElementOfPage(0).getText());
