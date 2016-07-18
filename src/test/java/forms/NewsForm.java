d037fcb620a99e21ada03dc0d06f22c7b171cebf
dzhigimont <dzhigimont@mail.ru>
2016-07-18 12:59:55 +0300

    Update NewsForm.java

--- src/test/java/forms/NewsForm.java
+++ src/test/java/forms/NewsForm.java
@@ -10,9 +10,20 @@ import webdriver.elements.ListOfPages;
 public class NewsForm extends BaseForm {
     private Label commentNumber = new Label(By.xpath("//a[@class='comment-icon-1']/span"),"commentNumber");
     private ListOfPages commentList = new ListOfPages(By.xpath("//span[@class='date']"),"commentList");
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public NewsForm() {
         super(By.xpath("//h3[contains(text(),'Обсуждение')]"), "News.Logo");
     }
+    
+    /**
+     * assertComments()
+     *this method considers and compares the number of comments
+     * @return void
+     */
     public void assertComments(){
         commentNumber.navigateToElement();
         String msgPass = "Displays the number = "+commentNumber.getText()+ " of matches counted the number = "+commentList.getListOfPageSize();
