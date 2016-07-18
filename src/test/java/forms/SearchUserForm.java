6d7aebeae4a0e400174d5db54d6ff185dd9a2e43
dzhigimont <dzhigimont@mail.ru>
2016-07-18 13:09:38 +0300

    Update SearchUserForm.java

--- src/test/java/forms/SearchUserForm.java
+++ src/test/java/forms/SearchUserForm.java
@@ -5,17 +5,27 @@ import webdriver.BaseForm;
 import webdriver.elements.Button;
 import webdriver.elements.TextBox;
 
-/**
- * Created by kristinka on 15.07.2016.
- */
+
 public class SearchUserForm extends BaseForm{
 
     private TextBox searchUser = new TextBox(By.xpath("//input[contains(@type,'text')]"),"searchUser");
     private Button find = new Button(By.xpath("//span[contains(text(),'Найти')]"),"find");
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public SearchUserForm(){
         super(By.xpath("//p[contains(text(),'Введите имя пользователя')]"),"SearchUser.Logo");
     }
-
+     /**
+     * searchUser()
+     *this method fills out a form to search user
+     *
+     * @param userName
+     *
+     * @return void
+     */
     public void searchUser(String userName){
         searchUser.type(userName);
         find.clickAndWait();
