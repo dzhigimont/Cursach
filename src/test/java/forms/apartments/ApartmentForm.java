879d0e73e07d9fba9b27971113c1f4f18a34555d
dzhigimont <dzhigimont@mail.ru>
2016-07-18 12:34:45 +0300

    Update ApartmentForm.java

--- src/test/java/forms/apartments/ApartmentForm.java
+++ src/test/java/forms/apartments/ApartmentForm.java
@@ -12,12 +12,21 @@ public class ApartmentForm extends BaseForm {
 
     private Label street =  new Label(By.xpath("//div[contains(@class,'apartment-info__sub-line_large')][not(child::ul)]"),"Street");
     private Label streetCoordinates = new Label(By.xpath("//div[@id='map']/following-sibling::script"),"StreetCoordinates");
-
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public ApartmentForm(){
         super(By.xpath("//div[contains(@class,'leaflet-marker-icon')]"),"Apartment.Logo");
     }
 
-
+    **
+     * getStreet()
+     *this method find addres in the catalog
+     *
+     * @return Strit = 'Minsk'
+     */
     public String getStreet(){
         street.navigateToElement();
         return street.getText();
@@ -25,7 +34,12 @@ public class ApartmentForm extends BaseForm {
     }
 
 
-
+    **
+     * getCoordinate()
+     *this method find coordinates by addres in the catalog
+     *@param product = 'Iphone 5'
+     * @return Strit[] 
+     */
     public String[] getCoordinate(){
         Pattern pat = Pattern.compile("[0-9]+.[0-9]+");
         Matcher m = pat.matcher(browser.getDriver().findElement(streetCoordinates.getLocator()).getAttribute("text"));
@@ -41,7 +55,13 @@ public class ApartmentForm extends BaseForm {
         return parse;
     }
 
-
+    **
+     * ssertCoordinate()
+     *this method assert coordinate in the catalog with coordinate in other site
+     *@param arseOnliner 
+     @param parseMap
+     * @return void
+     */
     public  void assertCoordinate(String[] parseOnliner,String[] parseMap){
         for (int i=0;i<2;i++){
 
