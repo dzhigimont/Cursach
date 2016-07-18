91b2037be47fc74d9d6d60f5acca5f0232ab5887
dzhigimont <dzhigimont@mail.ru>
2016-07-18 12:36:22 +0300

    Update ApartmentsSearchForm.java

--- src/test/java/forms/apartments/ApartmentsSearchForm.java
+++ src/test/java/forms/apartments/ApartmentsSearchForm.java
@@ -8,12 +8,21 @@ import webdriver.elements.ListOfPages;
 public class ApartmentsSearchForm extends BaseForm {
 
     private ListOfPages apartmentsList = new ListOfPages(By.xpath("//a[@data-id]"),"ApartmentsList");
-
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public ApartmentsSearchForm(){
         super(By.xpath("//span[contains(text(),'Аренда')]"),"ApartmentsSearchForm.Logo");
 
     }
-
+     /**
+     * clickFirstLink( )
+     *this method click first link in the searh box
+     *
+     * @return void
+     */
     public void clickFirstLink( ){
         apartmentsList.clickAndWait(0);
     }
