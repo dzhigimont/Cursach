16dd79f43df9be26bcf89b16a2de27421544b622
dzhigimont <dzhigimont@mail.ru>
2016-07-18 13:55:30 +0300

    Update DropDownMenuApartments.java

--- src/test/java/forms/apartments/DropDownMenuApartments.java
+++ src/test/java/forms/apartments/DropDownMenuApartments.java
@@ -5,17 +5,8 @@ import org.openqa.selenium.By;
 import webdriver.BaseForm;
 import webdriver.elements.Button;
 
-public class DropDownMenuApartments extends BaseForm {
+public class DropDownMenuApartments {
     
-    /**
-     *
-     *this Class constructor
-     *
-     */
-    private Button minsk = new Button(By.xpath("//a[@class='b-main-navigation__dropdown-advert-link']/span[contains(text(),'Минск')]"),"Minsk");
-    public DropDownMenuApartments(){
-        super(By.xpath("//a[contains(text(),'Аренда')]"),"DropDownMenuApartments.Logo");
-    }
      /**
      * clickMinsk()
      *this method click Minsk from DropDownMenuApartments, becase not realese method navigate
