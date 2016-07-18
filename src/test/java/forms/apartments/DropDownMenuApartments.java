1cfc625a2d9ece319160eb3747552b73533f79fe
dzhigimont <dzhigimont@mail.ru>
2016-07-18 14:02:47 +0300

    Update DropDownMenuApartments.java

--- src/test/java/forms/apartments/DropDownMenuApartments.java
+++ src/test/java/forms/apartments/DropDownMenuApartments.java
@@ -7,6 +7,7 @@ import webdriver.elements.Button;
 
 public class DropDownMenuApartments {
     
+    private Button minsk = new Button(By.xpath("//a[@class='b-main-navigation__dropdown-advert-link']/span[contains(text(),'Минск')]"),"Minsk");
      /**
      * clickMinsk()
      *this method click Minsk from DropDownMenuApartments, becase not realese method navigate
