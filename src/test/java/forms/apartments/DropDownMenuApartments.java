a4ca842fea5d7d79931722ad300bfd01b7a8dea7
dzhigimont <dzhigimont@mail.ru>
2016-07-18 12:38:07 +0300

    Update DropDownMenuApartments.java

--- src/test/java/forms/apartments/DropDownMenuApartments.java
+++ src/test/java/forms/apartments/DropDownMenuApartments.java
@@ -6,11 +6,23 @@ import webdriver.BaseForm;
 import webdriver.elements.Button;
 
 public class DropDownMenuApartments extends BaseForm {
-
+    
+    /**
+     *
+     *this Class constructor
+     *
+     */
     private Button minsk = new Button(By.xpath("//a[@class='b-main-navigation__dropdown-advert-link']/span[contains(text(),'Минск')]"),"Minsk");
     public DropDownMenuApartments(){
         super(By.xpath("//a[contains(text(),'Аренда')]"),"DropDownMenuApartments.Logo");
     }
+     /**
+     * clickMinsk()
+     *this method click Minsk from DropDownMenuApartments, becase not realese method navigate
+     *
+     * @return void
+     */
+
     public void clickMinsk(){
         minsk.clickAndWait();
     }
