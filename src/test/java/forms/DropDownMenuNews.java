5cf324c1f8a0c07edb388e58079687e7e9a68350
dzhigimont <dzhigimont@mail.ru>
2016-07-18 12:48:11 +0300

    Update DropDownMenuNews.java

--- src/test/java/forms/DropDownMenuNews.java
+++ src/test/java/forms/DropDownMenuNews.java
@@ -7,9 +7,21 @@ import webdriver.elements.Button;
 public class DropDownMenuNews extends BaseForm{
 
     private Button technology = new Button(By.xpath("//div[@class='b-main-navigation__dropdown-title']/a[contains(text(),'Технологии')]"),"Technology");
+    
+    /**
+     *
+     *this Class constructor
+     *
+     */
     public DropDownMenuNews(){
         super(By.xpath("//a[contains(text(),'Перейти в раздел')]"),"DropDownMenuNews.Logo");
     }
+    /**
+     *clickTechnology
+     *this method click button Technology
+     *
+     * @return void
+     */
     public void clickTechnology(){
         technology.clickAndWait();
     }
