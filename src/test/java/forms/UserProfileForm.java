4b24e6fb708e1492d91b0392be1eab28eea5c0f4
dzhigimont <dzhigimont@mail.ru>
2016-07-18 13:10:08 +0300

    Update UserProfileForm.java

--- src/test/java/forms/UserProfileForm.java
+++ src/test/java/forms/UserProfileForm.java
@@ -3,12 +3,13 @@ package forms;
 import org.openqa.selenium.By;
 import webdriver.BaseForm;
 
-/**
- * Created by kristinka on 15.07.2016.
- */
 public class UserProfileForm extends BaseForm {
 
-
+/**
+     *
+     *this Class constructor
+     *
+     */
     public UserProfileForm(){
         super(By.xpath("//dt[contains(text(),'Учетная запись')]"),"UserProfile.Logo");
     }
